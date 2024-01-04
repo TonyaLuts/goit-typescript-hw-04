69a72a0c711c613be2ac1caa0dec8d70a25e7c34
TonyaLuts <131724958+TonyaLuts@users.noreply.github.com>
2024-01-04 19:21:52 +0000

    TS-4(2)

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -32,7 +32,9 @@ type PropsProvider = {
 
 function MenuProvider({ children }: PropsProvider) {
   // Додати тип для SelectedMenu він повинен містити { id }
-  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({});
+  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({
+    id: "first",
+  });
 
   const menuContextAction = useMemo(
     () => ({
